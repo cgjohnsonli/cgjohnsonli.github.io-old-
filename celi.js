90d23cfd6122dfe046eaee8ab0ea3a2f74a93a96
cgjohnsonli <richardsilkli@outlook.com>
2017-06-16 01:53:16 -0400

    animation-frame

--- celi.js
+++ celi.js
@@ -9,24 +9,110 @@ $(function(){
 });
 
 $(function(){
-	$('a#archall').click(function(){
-	$(".list-obj").fadeIn("slow");
-	return false;
+	
+	var $data=$("#arch-content").clone();
+	var $source=$data.find('li.archac, li.archpr, li.archco');
+	$("#arch-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
+		
+	$("ul#arch-nav li").click(function(){
+	var id = $(this).attr("id");
+	if(id=="archall"){
+		var $source=$data.find('li.archac, li.archpr, li.archco');
+	}else{
+		var $source=$data.find("li."+id);
+	}
+	
+	$("#arch-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
 	});
 
-	$('a#archac, a#archpr, a#archco').click(function(){
-	var fa = $(this).attr("id");
-	$(".list-obj").fadeOut(100);
-	$("."+fa).fadeIn("slow");
-	return false;
+});
+
+
+$(function(){
+	
+	var $data=$("#code-content").clone();
+	var $source=$data.find('li.codefa, li.codere, li.codeap');
+	$("#code-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
+		
+	$("ul#code-nav li").click(function(){
+	var id = $(this).attr("id");
+	if(id=="codeall"){
+		var $source=$data.find('li.codefa, li.codere, li.codeap');
+	}else{
+		var $source=$data.find("li."+id);
+	}
+	
+	$("#code-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
 	});
+
 });
 
 $(function(){
-	$(".list-img").mouseover(function(){
-	$(this).fadeTo("fast",0.2);
+	
+	var $data=$("#media-content").clone();
+	var $source=$data.find('li.mediaweb, li.mediagame, li.mediaorg');
+	$("#media-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
+		
+	$("ul#media-nav li").click(function(){
+	var id = $(this).attr("id");
+	if(id=="mediaall"){
+		var $source=$data.find('li.mediaweb, li.mediagame, li.mediaorg');
+	}else{
+		var $source=$data.find("li."+id);
+	}
+	
+	$("#media-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
 	});
-	$(".list-img").mouseout(function(){
-	$(this).fadeTo("fast",1);
+
+});
+
+$(function(){
+	
+	var $data=$("#contact-content").clone();
+	var $source=$data.find('li.contactgit, li.contactmail, li.mediaorg');
+	$("#contact-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
+		
+	$("ul#contact-nav li").click(function(){
+	var id = $(this).attr("id");
+	if(id=="mediaall"){
+		var $source=$data.find('li.contactgit, li.contactmail, li.mediaorg');
+	}else{
+		var $source=$data.find("li."+id);
+	}
+	
+	$("#contact-content").quicksand($source,{
+	duration: 500,
+	attribute: 'id',
+	easing: 'swing',
+		});
 	});
+
 });
